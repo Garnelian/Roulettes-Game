97a028f36515e109aca4d3127e04dd151e31434e
Camilo Jimenez <camilo.jimenez@dreamcodesoft.com>
2025-02-26 23:05:33 -0500

    Initial Tests Project Schema Database Entity Framwork In Memory.

---
+++ RoullettesGameTest/CustomWebApplicationFactory.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using RoulettesGame.Data;
+using System.Linq;
+
+public class CustomWebApplicationFactory<TProgram> : WebApplicationFactory<TProgram> where TProgram : class
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            var descriptor = services.SingleOrDefault(
+            d => d.ServiceType ==
+            typeof(IDbContextOptionsConfiguration<ApplicationDbContext>));
+
+            if (descriptor != null)
+            {
+                services.Remove(descriptor);
+            }
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseInMemoryDatabase("TestDb");
+            });
+
+            var serviceProvider = services.BuildServiceProvider();
+
+        });
+    }
+}
